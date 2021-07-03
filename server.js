7a612487dbb1389b02397be9b4371984b0299f36
Rahul Vij <vijrahul5>
2021-07-03 21:47:47 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/jira",jiraRouter);
 app.use("/api/auth", authRouter); // All routes with '/api/auth' will be redirected to authRouter
 app.use(protectRoute); // Middleware for protecting access to apis which require the user to be logged in
+app.use("/api/jira",jiraRouter);
 app.use("/api/employee", employeeRouter); // All routes with '/api/employee' will be redirected to employeehRouter
 
 let port = process.env.PORT;
