c7f963eb2e3cc5565d5051b668681dd6e9b2bcbe
Rahul Vij <vijrahul5>
2021-07-05 11:27:29 +0530

    Changes to Jira Controller

--- controller/jiraController.js
+++ controller/jiraController.js
@@ -1,29 +1,34 @@
 const Pusher = require("pusher");
+require("dotenv").config();
 
 const pusher = new Pusher({
-    appId: "",
-    key: "",
-    secret: "",
-    cluster: "",
-    useTLS: true,
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: "ap2",
+  useTLS: true,
 });
 
-function triggerChannel(obj) {
-    console.log("Channel Triggered");
-    pusher.trigger("my-channel", obj.cid, {
-        details: obj,
+function triggerChannel(obj, matchedWebhookIds) {
+  // console.log("Channel Triggered");
+  matchedWebhookIds.forEach((element) => {
+    pusher.trigger("my-channel", `${obj.cid}${element}`, {
+      details: obj,
     });
+  });
 }
 
 function handleNotification(req, res) {
-    res.sendStatus(200);
-    let obj = {
-        type: req.body.issue_event_type_name,
-        userId: req.body.user.accountId,
-        key: req.body.issue.key,
-        cid: req.query.cid,
-    };
-    triggerChannel(obj);
+  res.sendStatus(200);
+  // console.log(req);
+  let obj = {
+    type: req.body.issue_event_type_name,
+    userId: req.body.user.accountId,
+    key: req.body.issue.key,
+    cid: req.query.cid,
+  };
+  // console.log(req.body.timestamp, obj);
+  triggerChannel(obj, req.body.matchedWebhookIds);
 }
 
 module.exports.handleNotification = handleNotification;
